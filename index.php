ae487c96037052d98d41be27cfee332042ee5c38
Superstar-IT <freewebdeveloper@hotmail.com>
2020-01-18 06:03:48 +0800

    'c'

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
-<!DOCTYPE html>
-<html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <script src="js/latest-v2.js"></script> 
-    </head>
-    <body>
-        <video height="300" id="localVideo" controls></video>
-        <!-- <div id="remotesVideos"></div> -->
-    </body>
-</html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<div id='page-wrapper'>
+    <div class="container-fluid">
+        <div class="row bg-title">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h4 class="page-title">Con X Live</h4>
+            </div>
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                <ol class="breadcrumb">
+                </ol>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xs-12">
+                <div class="white-box">
+                    <h3 class="box-title">
+                        <i class="icon-camrecorder p-r-10"></i> 
+                    </h3>
+                    <video height="300" id="localVideo" style="width:100%; height: 70vh;"></video>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="js/latest-v2.js"></script>
 <script type="text/javascript" charset="utf-8" async defer>
   var webrtc = new SimpleWebRTC({
     // the id/element dom element that will hold "our" video
@@ -18,18 +32,10 @@
     // immediately ask for camera access
     autoRequestMedia: true
   });
-
+  console.log(webrtc);
   // we have to wait until it's ready
   webrtc.on('readyToCall', function () {
     // you can name it anything
     webrtc.joinRoom('roam');
-    // $('#localVideo').click();
   });
-  
-  // $(document).ready(function(){
-  //   document.addEventListener('click', event => {
-  //     const video = document.getElementById('localVideo');
-  //     video.play();
-  //   });
-  // });
 </script>
