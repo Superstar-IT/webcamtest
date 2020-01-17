39089d155cfc426981682e00fd96f6accec36f1e
Superstar-IT <freewebdeveloper@hotmail.com>
2020-01-18 00:41:42 +0800

    'test'

--- index.php
+++ index.php
@@ -3,24 +3,9 @@
         <script src="js/latest-v2.js"></script> 
     </head>
     <body>
-        <video height="300" id="localVideo"></video>
+        <video height="300" id="localVideo">
+          <source src=http://techslides.com/demos/sample-videos/small.mp4 type=video/mp4>
+        </video>
         <!-- <div id="remotesVideos"></div> -->
     </body>
 </html>
-
-<script type="text/javascript" charset="utf-8" async defer>
-  var webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remotesVideos',
-    // immediately ask for camera access
-    autoRequestMedia: true
-  });
-
-  // we have to wait until it's ready
-  webrtc.on('readyToCall', function () {
-    // you can name it anything
-    webrtc.joinRoom('roam');
-  });
-</script>
