9562d9de3723e49d16c6b66968238964a7d64399
Superstar-IT <freewebdeveloper@hotmail.com>
2020-01-18 00:07:32 +0800

    'new'

---
+++ livestreamview.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script src="js/latest-v2.js"></script> 
+    </head>
+    <body>
+        <div id="remotesVideos"></div>
+    </body>
+</html>
+
+<script type="text/javascript" charset="utf-8" async defer>
+  var webrtc = new SimpleWebRTC({
+    // the id/element dom element that will hold "our" video
+    localVideoEl: 'localVideo',
+    // the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remotesVideos',
+    // immediately ask for camera access
+    autoRequestMedia: true
+  });
+
+  // we have to wait until it's ready
+  webrtc.on('readyToCall', function () {
+    // you can name it anything
+    webrtc.joinRoom('roam');
+  });
+</script>
